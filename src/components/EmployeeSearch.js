1af9385b11bd65c70f7f0930e90ec82f30f99749
vall4251n <58278138+nvalline@users.noreply.github.com>
2020-07-15 14:54:36 -0600

    added search styles

--- src/components/EmployeeSearch.js
+++ src/components/EmployeeSearch.js
@@ -3,13 +3,16 @@ import React from 'react'
 function EmployeeSearch(props) {
     return (
         <form>
-            <input
-                type="text"
-                placeholder="Enter Employee Name"
-                name="search"
-                value={props.value}
-                onChange={props.handleInputChange}
-            />
+            <div className="form-group my-5 mx-auto">
+                <input
+                    type="text"
+                    placeholder="Enter Employee Name"
+                    name="search"
+                    value={props.value}
+                    onChange={props.handleInputChange}
+                    className="form-control"
+                />
+            </div>
         </form>
     )
 }
